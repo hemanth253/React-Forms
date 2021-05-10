e273ce6a80300f2b079a3bd0f66075785b333c2e
hemanth253 <83533561+hemanth253@users.noreply.github.com>
2021-05-10 17:34:56 +0530

    v1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  function handler() {
-    console.log("Changed");
+  const [name, setName] = useState("");
+  const [headingText, setHeading] = useState("");
+
+  function handleChange(event) {
+    console.log(event.target.value);
+    setName(event.target.value);
   }
+
+  function handleClick(event) {
+    setHeading(name);
+
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input onChange={handler} type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {headingText}</h1>
+      <form onSubmit={handleClick}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
