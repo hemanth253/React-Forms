6e3c09a1f18925a09751ffa7e1bfe9abf26babbb
hemanth253 <83533561+hemanth253@users.noreply.github.com>
2021-05-10 16:11:16 +0530

    v3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 function App() {
+  function handler() {
+    console.log("Changed");
+  }
   return (
     <div className="container">
       <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
+      <input onChange={handler} type="text" placeholder="What's your name?" />
       <button>Submit</button>
     </div>
   );
